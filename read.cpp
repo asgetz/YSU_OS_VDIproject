39a0b7f0ef5c17e1da81120c2b916232eff1ad4b
root <root@MSI.localdomain>
2018-01-31 21:44:32 -0500

    Initial repository set up; nothing finished

---
+++ read.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   read.cpp
+ * Author: getza
+ *
+ * Created on January 31, 2018, 8:44 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
